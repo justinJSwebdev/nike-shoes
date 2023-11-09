0191e2dfb0e3da96f49729133d002414b52609b3
tran ky hung <trankyhung225@gmail.com>
2023-11-09 18:12:05 +0700

    shoes updated ui

--- src/apis/user.api.ts
+++ src/apis/user.api.ts
@@ -2,10 +2,6 @@ import http from 'src/http'
 import { LoginData, RegisterData } from 'src/types/Auth.type'
 import { ResponseData } from 'src/types/Response.type'
 const URL = '/'
-type registerSuccess = {
-  token: string
-  status: string
-}
 
 // export const loginAPI = (data: LoginData) => http.post<ResponseData<AuthData>>(`${URL}login`, data)
 // export const signUpAPI = (data: RegisterData) => http.post<ResponseData<AuthData>>(`${URL}register`, data)
